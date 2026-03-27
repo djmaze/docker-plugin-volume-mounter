ef90bf57880ed0952da45b3aa5df2986e8ce0777
djmaze <7229+djmaze@users.noreply.github.com>
2026-03-27 14:26:32 +0000

    Skip volumes with empty mountpoint

--- main.go
+++ main.go
@@ -54,10 +54,14 @@ func (mounter Mounter) MountCurrentVolumes() {
 
   for _, volume := range volumes.Volumes {
     if volume.Driver != "local" {
-			if _, err := os.Stat(volume.Mountpoint); os.IsNotExist(err) {
-			  fmt.Printf("Volume mountpoint %s does not exist, skipping volume %s\n", volume.Mountpoint, volume.Name)
-				continue
-			}
+      if volume.Mountpoint == "" {
+        fmt.Printf("Volume mountpoint is empty, skipping volume %s\n", volume.Name)
+        continue
+      }
+      if _, err := os.Stat(volume.Mountpoint); os.IsNotExist(err) {
+        fmt.Printf("Volume mountpoint %s does not exist, skipping volume %s\n", volume.Mountpoint, volume.Name)
+      	continue
+      }
 
       volume_path := mounter.getVolumePath(ctx, volume)
       fmt.Println("Checking for volume path " + volume_path)
